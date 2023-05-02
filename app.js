4695374896f1d4badf66451d40f2fbe5f60812c3
Joel Hemmelgarn <jdhemmelgarn@gmail.com>
2023-05-02 11:31:26 -0400

    Updated JS

--- app.js
+++ app.js
@@ -1,111 +1,75 @@
 const calculator = document.querySelector('.calculator');
-const display = document.querySelector('.display');
+const equation = calculator.querySelector('.equation');
+const display = calculator.querySelector('.display');
 const keys = calculator.querySelector('.calc-keys');
 
-const firstValue = '';
-const operator = '';
-const secondValue = '';
+keys.addEventListener('click', event => {
+    
+    if (!event.target.closest('button')) return;
 
-const add = (num1, num2) => {
-    let result = num1 + num2;
-    return result;
-}
+    const key = event.target;
+    const keyValue = key.textContent;
+    const displayValue = display.textContent;
+    const { type } = key.dataset;
+    const { previousKeyType } = calculator.dataset;
 
-const subtract = (num1, num2) => {
-    let result = num1 - num2;
-    return result;
-}
 
-const multiply = (num1, num2) => {
-    let result = num1 * num2;
-    return result;
-}
+    if (type == 'number') {
+        if (displayValue == '0') {
+            display.textContent = keyValue; 
+        } else if(previousKeyType == 'operator') {
+            display.textContent = keyValue;
+        } else {
+            display.textContent = displayValue + keyValue;
+        }
+    }
 
-const divide = (num1, num2) => {
-    let result = num1 / num2;
-    return result;
-}
+    if (type == 'operator') {
+        const operatorKeys = keys.querySelectorAll('[data-type="operator"]');
+        operatorKeys.forEach(key => { 
+            key.dataset.state = ''
+        });
+        key.dataset.state = 'selected';
 
-const operate = (firstValue, operator, secondValue) => {
+        calculator.dataset.firstNumber = displayValue;
+        calculator.dataset.operator = key.dataset.key;
+    }
+
+    if (type == 'equal') {
+        //Perform a calculation
+        const firstNumber = calculator.dataset.firstNumber;
+        const operator = calculator.dataset.operator;
+        const secondNumber = displayValue;
+        display.textContent = operate(firstNumber, operator, secondNumber);
+    }
+
+    calculator.dataset.previousKeyType = type;
+})
+
+function operate (firstNumber, operator, secondNumber) {
+    firstNumber = parseInt(firstNumber);
+    secondNumber = parseInt(secondNumber);
+
     let result = '';
+    if (operator == 'plus') {
+        result = firstNumber + secondNumber;
+        equation.textContent = `${firstNumber} + ${secondNumber}`;
+    }
 
-    if (operator === 'add') {
-        result = add(firstValue, secondValue);
-    } else if (operator === 'subtract') {
-        result = subtract(firstValue, secondValue);
-    } else if (operator === 'multiply') {
-        result = multiply(firstValue, secondValue);
-    } else if (operator === 'divide') {
-        result = divide(firstValue, secondValue);
+    if (operator == 'minus') {
+        result = firstNumber - secondNumber;
+        equation.textContent = `${firstNumber} - ${secondNumber}`;
+    }
+
+    if (operator == 'times') {
+        result = firstNumber * secondNumber;
+        equation.textContent = `${firstNumber} x ${secondNumber}`;
+    }
+
+    if (operator == 'divide') {
+        result = firstNumber / secondNumber;
+        equation.textContent = `${firstNumber} / ${secondNumber}`;
     }
 
     return result;
 }
-
-console.log(operate(9, 'add', 10));
-
-// keys.addEventListener('click', e => {
-//     if (e.target.matches('button')) {
-//         const key = e.target;
-//         const action = key.dataset.action;
-//         const keyContent = key.textContent;
-//         const displayedNum = display.textContent;
-//         const previousKeyType = calculator.dataset.previousKeyType;
-
-//         if (!action) {
-//             if (displayedNum === '0' || previousKeyType === 'operator') {
-//                 display.textContent = keyContent;
-//             } else {
-//                 display.textContent = displayedNum + keyContent;
-//             };
-//             calculator.dataset.previousKeyType = 'number';
-//         }
-
-//         if (action === 'add' || action === 'subtract' || action === 'multiply' || action === 'divide') {
-//             const firstValue = calculator.dataset.firstValue;
-//             const operator = calculator.dataset.operator;
-//             const secondValue = displayedNum;
-
-//             if (firstValue && operator && previousKeyType !== 'operator') {
-//                 const calcValue = operate(firstValue, operator, secondValue);
-//                 display.textContent = calcValue;
-
-//                 // Update calculated value as firstValue
-//                 calculator.dataset.firstValue = calcValue;
-//             } else {
-//                 // If there are no calculations, set displayeNum as the firstValue
-//                 calculator.dataset.firstValue = displayedNum;
-//             }
-
-//             key.classList.add('is-pressed');
-//             calculator.dataset.previousKeyType = 'operator';
-//             calculator.dataset.operator = action;
-//         }
-
-//         if (action === 'decimal') {
-//             if (!displayedNum.includes('.')) {
-//                 display.textContent = displayedNum + '.';
-//             } else if (previousKeyType === 'operator') {
-//                 display.textContent = '0.';
-//             }
-
-//             calculator.dataset.previousKeyType = 'decimal';
-//         }
-
-//         if (action === 'clear') {
-//             display.textContent = '0'
-//             calculator.dataset.previousKeyType = 'clear';
-//         }
-
-//         if (action === 'calculate') {
-//             const secondValue = displayedNum;
-//             const operator = calculator.dataset.operator;
-//             const firstValue = calculator.dataset.firstValue;
-
-//             display.textContent = operate(firstValue, operator, secondValue);
-//             calculator.dataset.previousKeyType = 'calculate';
-//         }
-
-//         Array.from(key.parentNode.children).forEach(k => k.classList.remove('is-pressed'))
-//     }
-// })
